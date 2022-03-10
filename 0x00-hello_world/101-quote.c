cc9bcea380ca453e6725d3610456549ebda350e6
MonahDipsy <98586425+MonahDipsy@users.noreply.github.com>
2022-03-10 14:48:16 +0300

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints exactly "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * 
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
