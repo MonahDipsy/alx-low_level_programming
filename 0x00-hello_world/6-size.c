f471aa988affc173379b569141a5308b24c55535
MonahDipsy <monicahodipo55@gmail.com>
2022-03-10 03:29:38 -0800

    ASize

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ *main - prints size of various types on computer compiled on
+ *
+ * Return: 0 if successful, otherwise not 0 unsuccessful
+ */
+int main(void)
+{
+	printf("Size of an int:%d byte(s)\n",sizeof(int));
