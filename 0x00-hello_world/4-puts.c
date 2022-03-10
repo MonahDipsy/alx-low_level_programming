c890ef764eb7a3975eeb07316c2f8ec92babdb42
MonahDipsy <98586425+MonahDipsy@users.noreply.github.com>
2022-03-10 13:07:28 +0300

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- *main - Print Programming is like building a multilingual puzzle
- * Return: 0 if Successful , otherwise Unsuccessful
+ * main - Programming is like building a multilingual puzzle
+ * Return: 0 if successful, non-zero otherwise
  */
 int main(void)
 {
