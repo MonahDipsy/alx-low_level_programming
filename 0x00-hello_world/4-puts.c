6400584b95e3ae09a15090d6e403c8daf86db842
MonahDipsy <monicahodipo55@gmail.com>
2022-03-10 01:26:21 -0800

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*Print Programming is like building a multilingual puzzle*/
+
+int main(void)
+{
+
+	puts("Programming is like building a multilingual puzzle"\n");
+	return 0;
+
+}	
