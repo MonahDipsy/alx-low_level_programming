f7e299215e85eedc3ee47db7313cef708920ea1b
MonahDipsy <monicahodipo55@gmail.com>
2022-03-11 05:34:05 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print Alphabet in lowercase
+ *
+ * Return: 0 for successful
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
