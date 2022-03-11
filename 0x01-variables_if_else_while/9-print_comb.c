061ac084b3396550e448ea3886cc07b0fbc5f056
MonahDipsy <monicahodipo55@gmail.com>
2022-03-11 09:30:47 -0800

    Possible combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combination of single digit numbers
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
