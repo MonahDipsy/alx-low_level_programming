c9e676869714ba98abb4568d40e55464101d5f98
MonahDipsy <monicahodipo55@gmail.com>
2022-03-15 12:27:56 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Print _putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char s[] = "_putchar";
+	int i = 0;
+
+	while (s[i] != '\n')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar(10);
+
+	return (0);
+}
