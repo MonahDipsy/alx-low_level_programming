bfa83bf8a06a5bbb8680be3d836045cd173c8ef5
MonahDipsy <monicahodipo55@gmail.com>
2022-03-15 16:10:21 -0700

    natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - Prints all natural numbers from n to 98
- * Return: void
+ * print_to_98 - Prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: print from this number
  */
+
 void print_to_98(int n)
 {
 	int i;
