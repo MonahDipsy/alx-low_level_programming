7980360709f39603d976818d77e0d7aa92950008
MonahDipsy <monicahodipo55@gmail.com>
2022-03-15 15:29:04 -0700

    Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 times table starting with 0
+ * Return: 0
+ */
+void times_table(void)
+{
+	int x = 0; /* factor */
+	int y; /* count */
+	int z; /* computed value */
+
+	while (x < 10)
+	{
+
