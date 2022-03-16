ffa46dfac38585d9098a550d4aa0716c2f61edab
MonahDipsy <monicahodipo55@gmail.com>
2022-03-16 14:45:17 -0700

    Comment

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,11 +11,11 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-	i = 0; /* don't delete it! */
+	i = 0; 
 
 	while (i < 10) 
 	{
-		putchar(i);
+		putchar(i); /* don't delete it! */
 	}
 
 	printf("Infinite loop avoided! \\o/\n");
